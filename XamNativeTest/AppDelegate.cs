294d211280df15980b1c323ce54e618829d3aca9
jaydeep-helpshift <jaydeep@helpshift.com>
2023-06-22 09:59:37 +0530

    First commit

---
+++ XamNativeTest/AppDelegate.cs
@@ -0,0 +1,49 @@
+﻿using Foundation;
+using UIKit;
+using HelpshiftApi;
+using System.Collections.Generic;
+
+namespace XamNativeTest
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register ("AppDelegate")]
+    public class AppDelegate : UIResponder, IUIApplicationDelegate {
+    
+        [Export("window")]
+        public UIWindow Window { get; set; }
+
+        [Export ("application:didFinishLaunchingWithOptions:")]
+        public bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+        {
+            Dictionary<string, object> config = new Dictionary<string, object>()
+            {
+                {"enableLogging", "true"}
+            };
+            //Helpshift.Install("gayatri_platform_20200925124520005-083e1cad798a089", "gayatri.helpshift.com", config);
+            HelpshiftCore.Initialize(HelpshiftApiProviderType.HelpshiftApiProviderTypeSupport);
+            var builder = new HelpshiftInstallConfig.Builder();
+            builder.SetEnableLogging(true);
+            HelpshiftCore.Install("e780dbb2a9c0eca1c32510e05736d7c1", "gayatri.helpshift.com", "gayatri_platform_20200925124520005-083e1cad798a089", builder.Build());
+            return true;
+        }
+
+        // UISceneSession Lifecycle
+
+        [Export ("application:configurationForConnectingSceneSession:options:")]
+        public UISceneConfiguration GetConfiguration (UIApplication application, UISceneSession connectingSceneSession, UISceneConnectionOptions options)
+        {
+            // Called when a new scene session is being created.
+            // Use this method to select a configuration to create the new scene with.
+            return UISceneConfiguration.Create ("Default Configuration", connectingSceneSession.Role);
+        }
+
+        [Export ("application:didDiscardSceneSessions:")]
+        public void DidDiscardSceneSessions (UIApplication application, NSSet<UISceneSession> sceneSessions)
+        {
+            // Called when the user discards a scene session.
+            // If any sessions were discarded while the application was not running, this will be called shortly after `FinishedLaunching`.
+            // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        }
+    }
+}
---
+++ XamNativeTest/ViewController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UIKit;
+using HelpshiftApi;
+
+namespace XamNativeTest
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var btn1 = UIButton.FromType(UIButtonType.System);
+            btn1.TranslatesAutoresizingMaskIntoConstraints = false;
+            btn1.SetTitle("Show Conversation", UIControlState.Normal);
+
+            btn1.TouchUpInside += (sender, e) => {
+                //Helpshift.ShowConversation(this, null);
+                HelpshiftSupport.ShowConversation(this, new System.Collections.Generic.Dictionary<string, object>());
+            };
+
+            var btn2 = UIButton.FromType(UIButtonType.System);
+            btn2.TranslatesAutoresizingMaskIntoConstraints = false;
+            btn2.SetTitle("Show FAQs", UIControlState.Normal);
+
+            btn2.TouchUpInside += (sender, e) => {
+                //Helpshift.ShowFAQs(this, null);
+                HelpshiftSupport.ShowFAQs(this, new System.Collections.Generic.Dictionary<string, object>());
+            };
+
+            var stack = new UIStackView(new UIView[] { btn1, btn2 });
+            stack.TranslatesAutoresizingMaskIntoConstraints = false;
+            stack.Axis = UILayoutConstraintAxis.Vertical;
+            stack.Alignment = UIStackViewAlignment.Center;
+            View.AddSubview(stack);
+            NSLayoutConstraint.ActivateConstraints(new NSLayoutConstraint[]
+            {
+                stack.CenterXAnchor.ConstraintEqualTo(View.CenterXAnchor),
+                stack.CenterYAnchor.ConstraintEqualTo(View.CenterYAnchor)
+            });
+        }
+    }
+}
